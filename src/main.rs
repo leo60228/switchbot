aac5b526ef816567623baf6f7a26997f671fd27a
leo60228 <iakornfeld@gmail.com>
2019-12-15 18:34:36 -0500

    frametime-based FPS

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ async fn main() {
     let mut capture = switchbot::start_capture().unwrap();
     let mut frames = 0;
     let start = Instant::now();
+    let mut last = start;
     while let Some(_) = capture.next().await {
         frames += 1;
-        let diff = (Instant::now() - start).as_secs_f64();
-        println!("{} frames in {} seconds ({}fps)", frames, diff, (frames as f64) / diff);
+        let now = Instant::now();
+        let diff = (now - start).as_secs_f64();
+        let time = (now - last).as_secs_f64();
+        let fps = 1.0 / time;
+        last = now;
+        println!("{} frames in {:.3} seconds ({:.3}fps)", frames, diff, fps);
     }
 }
